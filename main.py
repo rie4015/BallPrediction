fdbc957be81fa16d184e3cb7a95bc600efd979e1
rie4015 <rie4015@yandex.ru>
2024-06-26 18:24:20 +0500

    origin

---
+++ main.py
@@ -0,0 +1,10 @@
+import torch
+torch.cuda.is_available()
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
